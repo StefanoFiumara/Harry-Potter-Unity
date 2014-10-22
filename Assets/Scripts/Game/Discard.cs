8dfd344620d1e442af6694d993a7daff1ba3036b
Stefano Fiumara <sdf10517@gmail.com>
2014-10-22 13:55:40 -0500

    Begin Discard Pile implementation

---
+++ Assets/Scripts/Game/Discard.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Discard : MonoBehaviour {
+
+
+    private List<Transform> Cards;
+    public Player _Player; //not sure if needed just yet
+
+    private readonly Vector2 DISCARD_POSITION_OFFSET = new Vector2(-355f, -124); //to be determined
+    private readonly Vector2 DISCARD_SPACING = new Vector2(0, 0); //to be determined
+
+    
+	// Use this for initialization
+	void Start () {
+	    Cards = new List<Transform>();
+	}
+
+    public void Add(Transform card) //don't need to worry about type here, since they all go to the same place.
+    {
+        Cards.Add(card);
+
+
+    }
+}
