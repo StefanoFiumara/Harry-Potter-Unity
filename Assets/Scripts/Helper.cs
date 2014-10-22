06fdb1fa5736da51a1c86fe8a9d72686a646c76e
Stefano Fiumara <sdf10517@gmail.com>
2014-10-22 13:55:04 -0500

    Added Ggobal Helper  class

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helper {
+
+    public static void TweenCardToPosition(Transform card, Vector3 cardPosition, float tweenTime = 0.5f, bool rotate = true)
+    {
+        iTween.MoveTo(card.gameObject, iTween.Hash("time", tweenTime,
+                                                   "position", cardPosition,
+                                                   "easetype", iTween.EaseType.easeInOutSine,
+                                                   "islocal", true
+                                                   ));
+
+        if (rotate)
+        {
+            iTween.RotateTo(card.gameObject, iTween.Hash("time", tweenTime,
+                                                         "z", 270f,
+                                                         "easetype", iTween.EaseType.easeInOutSine,
+                                                         "islocal", true
+                                                         ));
+        }
+
+        iTween.ScaleTo(card.gameObject, iTween.Hash("x", 1, "y", 1, "time", tweenTime));
+    }
+}
