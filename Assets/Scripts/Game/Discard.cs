f8fe4ea561d09c87f5bb758f5875455827f6535a
Stefano Fiumara <sdf10517@gmail.com>
2014-11-05 15:47:08 -0600

    Added collider to Discard Pile, adjust  card state on Add

--- Assets/Scripts/Game/Discard.cs
+++ Assets/Scripts/Game/Discard.cs
@@ -8,12 +8,20 @@ public class Discard : MonoBehaviour {
     private List<Transform> Cards;
     public Player _Player; //not sure if needed just yet
 
-    private readonly Vector2 DISCARD_POSITION_OFFSET = new Vector2(-355f, -204); //to be determined
+    private readonly Vector2 DISCARD_POSITION_OFFSET = new Vector2(-355f, -204f); //to be determined
 
     
 	// Use this for initialization
 	void Start () {
 	    Cards = new List<Transform>();
+
+        if (gameObject.collider == null)
+        {
+            var col = gameObject.AddComponent<BoxCollider>();
+            col.isTrigger = true;
+            col.size = new Vector3(50f, 70f, 1f);
+            col.center = new Vector3(DISCARD_POSITION_OFFSET.x, DISCARD_POSITION_OFFSET.y, 0f);
+        }
 	}
 
     //TODO: Test this function
@@ -22,7 +30,7 @@ public class Discard : MonoBehaviour {
         Cards.Add(card);
         Vector3 cardPos = new Vector3(DISCARD_POSITION_OFFSET.x, DISCARD_POSITION_OFFSET.y, -16f);
         cardPos.z +=  Cards.Count * 0.2f;
-        Helper.TweenCardToPosition(card, cardPos, 0.5f, false); 
+        Helper.TweenCardToPosition(card, cardPos, GenericCard.CardStates.DISCARDED, 0.5f, false);
     }
 
     //TODO: OnMouseUp: View cards in pile
