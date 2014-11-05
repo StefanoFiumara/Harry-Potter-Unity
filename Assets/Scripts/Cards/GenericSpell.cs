65c23ae6c78f668eae6d5688e1c300029b55be46
Stefano Fiumara <sdf10517@gmail.com>
2014-11-05 15:46:24 -0600

    Added GenericSpell abstract class

---
+++ Assets/Scripts/Cards/GenericSpell.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using LessonTypes = Lesson.LessonTypes;
+
+public abstract class GenericSpell : GenericCard {
+
+    public LessonTypes CostType;
+    public int CostAmount;
+
+    public new void Start()
+    {
+        base.Start();
+    }
+
+    public void OnMouseUp()
+    {
+        if (State != CardStates.IN_HAND) return;
+
+        if (_Player.UseAction())
+        {
+            if (_Player.nLessonsInPlay >= CostAmount && _Player.LessonTypesInPlay.Contains(CostType))
+            {
+                _Player._Hand.Remove(transform);
+                //tween here
+                //TODO: need a special tween for spell cards.
+                OnPlayAction();
+            }
+        }
+    }
+
+    public abstract void OnPlayAction();
+}
