b18be5b0d90bb2ed62ee456c7a5dbe91fbf673ad
Stefano Fiumara <sdf10517@gmail.com>
2014-11-05 22:38:10 -0600

    Added PersistentCard interface
    All Cards which persist on the board (i.e. all cards with a horizontal
    orientation) should implement this.

---
+++ Assets/Scripts/Cards/PersistentCard.cs
@@ -0,0 +1,8 @@
+﻿public interface PersistentCard {
+
+    void OnInPlayBeforeTurnAction();
+    void OnInPlayAfterTurnAction();
+    void OnSelectedAction();
+    void OnEnterInPlayAction();
+    void OnExitInPlayAction();
+}
