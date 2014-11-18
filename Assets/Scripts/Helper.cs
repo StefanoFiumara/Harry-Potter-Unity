f2e6fdd2da902c89b33bae9c220d6c6a4f25967c
Stefano Fiumara <sdf10517@gmail.com>
2014-11-17 22:44:00 -0600

    Start work on tweenQueue

--- Assets/Scripts/Helper.cs
+++ Assets/Scripts/Helper.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 
 using CardStates = GenericCard.CardStates;
+using EaseType = iTween.EaseType;
 
-public class Helper {
+public class Helper : MonoBehaviour {
 
     public const int PREVIEW_LAYER = 9;
     public const int CARD_LAYER = 10;
@@ -14,8 +15,59 @@ public class Helper {
 
     public static Camera PreviewCamera;
     public static readonly Vector3 DefaultPreviewCameraPos = new Vector3(-400, 255, -70);
+    public static Queue<TweenObject> TweenQueue = new Queue<TweenObject>();
 
-    public static void TweenCardToPosition(Transform card, Vector3 cardPosition, CardStates stateAfterAnimation, float tweenDelay = 0f, iTween.EaseType easeType = iTween.EaseType.easeInOutSine)
+    private static bool TweenQueueRunning = false;
+
+    struct TweenObject
+    {
+        public GameObject target;
+        public float time;
+        public Vector3 position;
+        public EaseType easeType;
+        public float delay;
+    }
+
+    public static void AddTweenToQueue(GameObject target, Vector3 position, float time, float delay = 0f, EaseType easeType = EaseType.easeInOutSine)
+    {
+        TweenObject newTween;
+        newTween.target = target;
+        newTween.position = position;
+        newTween.time = time;
+        newTween.delay = delay;
+        newTween.easeType = easeType;
+
+        TweenQueue.Enqueue(newTween);
+
+        if (TweenQueueRunning == false)
+        {
+           // StartCoroutine(RunTweenQueue());
+        }
+    }
+
+    private static IEnumerator RunTweenQueue()
+    {
+        //I might break the game here
+        while (true)
+        {
+            if (TweenQueue.Count == 0) yield return null;
+
+            TweenObject tween = TweenQueue.Dequeue();
+
+            iTween.MoveTo(tween.target, iTween.Hash("time", tween.time,
+                                                   "position", tween.position,
+                                                   "easetype", tween.easeType,
+                                                   "islocal", true,
+                                                   "delay", tween.delay
+                                                   ));
+            yield return new WaitForSeconds(tween.time + tween.delay);
+
+        }
+    }
+
+
+
+    public static void TweenCardToPosition(Transform card, Vector3 cardPosition, CardStates stateAfterAnimation, float tweenDelay = 0f, EaseType easeType = iTween.EaseType.easeInOutSine)
     {
         iTween.MoveTo(card.gameObject, iTween.Hash("time", 0.5f,
                                                    "position", cardPosition,
@@ -36,10 +88,10 @@ public class Helper {
         Vector3 cardRotation = card.localRotation.eulerAngles;
         float target = cardRotation.z > 20f ? 0f : 270f;
         iTween.RotateTo(card.gameObject, iTween.Hash("time", 0.5f,
-                                                         "z", target,
-                                                         "easetype", iTween.EaseType.easeInOutSine,
-                                                         "islocal", true
-                                                         ));
+                                                     "z", target,
+                                                     "easetype", iTween.EaseType.easeInOutSine,
+                                                     "islocal", true
+                                                     ));
     }
 
     public static void DisableCards(List<GenericCard> cards)
