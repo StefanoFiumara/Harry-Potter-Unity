8790fd5bbbbc97dc4a4438182f5d824d48002c57
Stefano Fiumara <sdf10517@gmail.com>
2015-01-31 02:17:11 -0500

    main generation function

--- Assets/Scripts/HarryPotterUnity/Utils/DeckGenerator.cs
+++ Assets/Scripts/HarryPotterUnity/Utils/DeckGenerator.cs
@@ -1,37 +1,68 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Assets.Scripts.HarryPotterUnity.Cards;
-using UnityEditor;
 using UnityEngine;
 
 namespace Assets.Scripts.HarryPotterUnity.Utils
 {
     public class DeckGenerator
     {
-        public static List<GenericCard> CardLibrary { get; set; }
+        private static List<GenericCard> _cardLibrary;
+
+        public static List<GenericCard> CardLibrary
+        {
+            get {
+                    if (_cardLibrary == null) LoadCardLibrary();
+                    return _cardLibrary;
+                }
+        }
 
         private static void LoadCardLibrary()
         {
-            CardLibrary = new List<GenericCard>();
+            _cardLibrary = new List<GenericCard>();
 
-            var prefabPaths =
-                AssetDatabase.GetAllAssetPaths()
-                    .Where(path => path.EndsWith(".prefab") && path.Contains("Resources/Cards/"));
-
-            foreach (var path in prefabPaths)
+            var resources = Resources.LoadAll("Cards/");
+            
+            foreach (var container in resources.Cast<GameObject>())
             {
-                var container = AssetDatabase.LoadAssetAtPath(path, typeof (GameObject)) as GameObject;
                 if (container == null)
                 {
-                    Debug.LogError(string.Format("Failed to load asset at: {0}", path));
+                    Debug.LogError("Failed to load asset");
                     continue;
                 }
                 var cardInfo = container.GetComponent<GenericCard>();
-                CardLibrary.Add(cardInfo);
+                _cardLibrary.Add(cardInfo);
             }
         }
 
-        private void AddLessonsToDeck(ref List<GenericCard> deck, Lesson.LessonTypes lessonType, int amount)
+        public List<GenericCard> GenerateDeck(List<Lesson.LessonTypes> types)
+        {
+            if (types.Count != 2 && types.Count != 3)
+            {
+                throw new Exception(types.Count + " type(s) sent to GenerateDeck, unsupported");
+            }
+
+            var deck = new List<GenericCard>();
+
+            switch (types.Count)
+            {
+                case 2:
+                    AddLessonsToDeck(ref deck, types[0], 15);
+                    AddLessonsToDeck(ref deck, types[1], 15);
+                    break;
+                case 3:
+                    AddLessonsToDeck(ref deck, types[0], 15);
+                    AddLessonsToDeck(ref deck, types[1], 8);
+                    AddLessonsToDeck(ref deck, types[2], 7);
+                    break;
+            }
+            //TODO: Finish this function
+
+            return deck;
+        }
+
+        private static void AddLessonsToDeck(ref List<GenericCard> deck, Lesson.LessonTypes lessonType, int amount)
         {
             var card = CardLibrary.Where(c => c.Classification == GenericCard.ClassificationTypes.Lesson)
                 .First(l => ((Lesson) l).LessonType == lessonType);
