7564de0d5126ec66561d63cf334da6cea0a25d27
Stefano Fiumara <sdf10517@gmail.com>
2015-02-01 20:28:15 -0500

    Import Photon Networking

---
+++ Assets/Scripts/HarryPotterUnity/Utils/NetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.HarryPotterUnity.Utils
+{
+    public class NetworkManager : MonoBehaviour
+    {
+
+        [SerializeField] private Text _networkStatusText;
+
+        public void Start ()
+        {
+            PhotonNetwork.ConnectUsingSettings("v0.1");
+        }
+
+        public void FixedUpdate()
+        {
+            _networkStatusText.text = string.Format("Network Status: {0}", PhotonNetwork.connectionStateDetailed);
+        }
+
+        public void OnJoinedLobby()
+        {
+            
+        }
+    }
+}
