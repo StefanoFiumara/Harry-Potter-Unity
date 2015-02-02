9b54d8280ee1dd1590d881a7df197329bcaa801b
Stefano Fiumara <sdf10517@gmail.com>
2015-02-02 01:06:23 -0500

    matchmaking connects clients

--- Assets/Scripts/HarryPotterUnity/Utils/NetworkManager.cs
+++ Assets/Scripts/HarryPotterUnity/Utils/NetworkManager.cs
@@ -7,6 +7,14 @@ namespace Assets.Scripts.HarryPotterUnity.Utils
     public class NetworkManager : MonoBehaviour
     {
         [SerializeField] private Text _networkStatusText;
+        private bool _connected = false;
+
+        public enum NetworkState
+        {
+            Disconnected, InLobby, WaitingForMatch, InGame
+        }
+
+        private NetworkState _networkState = NetworkState.Disconnected;
 
         public void Start ()
         {
@@ -20,7 +28,59 @@ namespace Assets.Scripts.HarryPotterUnity.Utils
 
         public void OnJoinedLobby()
         {
-            
+            _networkState = NetworkState.InLobby;
+        }
+
+        public void OnJoinedRoom()
+        {
+            _networkState = PhotonNetwork.room.playerCount >= 2 ? NetworkState.InGame : NetworkState.WaitingForMatch;
+        }
+
+        public void OnPhotonPlayerConnected()
+        {
+            _networkState = PhotonNetwork.room.playerCount >= 2 ? NetworkState.InGame : NetworkState.WaitingForMatch;
+
+            //start game here?
+        }
+
+        public void OnGUI()
+        {
+            switch (_networkState)
+            {
+                case NetworkState.InLobby:
+                    ShowMainMenu();
+                    break;
+                case NetworkState.WaitingForMatch:
+                    ShowWaiting();
+                    break;
+                case NetworkState.InGame:
+                    ShowStartingGame();
+                    break;
+            }
+        }
+
+        private void ShowStartingGame()
+        {
+            GUI.Label(new Rect(100,100,190,90), "Match found! Joining Game...");
+        }
+
+        private void ShowWaiting()
+        {
+            GUI.Label(new Rect(100,100,190,90), "Waiting for match...");
+        }
+
+        private void ShowMainMenu()
+        {
+            if (GUI.Button(new Rect(100, 100, 190, 90), "Find Match..."))
+            {
+                _networkState = NetworkState.WaitingForMatch;
+                PhotonNetwork.JoinRandomRoom();
+            }
+        }
+
+        public void OnPhotonRandomJoinFailed()
+        {
+            PhotonNetwork.JoinOrCreateRoom(string.Format("Room {0}", PhotonNetwork.GetRoomList().Length),  null, null);
         }
     }
 }
