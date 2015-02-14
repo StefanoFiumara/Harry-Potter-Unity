c1feb1d01011be7ca9b1cb783a2799fd45da423b
Stefano Fiumara <sdf10517@gmail.com>
2015-02-14 15:00:01 -0600

    add network view stub

---
+++ Assets/Scripts/HarryPotterUnity/Utils/PlayerNetworkView.cs
@@ -0,0 +1,33 @@
+﻿namespace HarryPotterUnity.Utils
+{
+    public class PlayerNetworkView : Photon.MonoBehaviour
+    {
+
+        void Start()
+        {
+       
+        }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.isWriting)
+            {
+                // We own this player: send the others our data
+
+            }
+            else
+            {
+                // Network player, receive data
+
+            }
+        }
+
+        void Update()
+        {
+            if (!photonView.isMine)
+            {
+            
+            }
+        }
+    }
+}
