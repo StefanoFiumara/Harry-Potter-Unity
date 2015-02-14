811575de20a7b30c28dd0bce5d9f6cd84cdee53e
Stefano Fiumara <sdf10517@gmail.com>
2015-02-14 15:01:04 -0600

    add startgame() to gamemanager

--- Assets/Scripts/HarryPotterUnity/Game/GameManager.cs
+++ Assets/Scripts/HarryPotterUnity/Game/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using HarryPotterUnity.Cards;
 using HarryPotterUnity.Utils;
@@ -9,20 +10,22 @@ namespace HarryPotterUnity.Game
     public class GameManager : MonoBehaviour
     {
         //TODO: Are these references kept between clients?
-        public Player Player1 { get; set; }
-        public Player Player2 { get; set; }
+        public Player Player1;
+        public Player Player2;
         
-        private IEnumerator StartGame()
+        public void StartGame()
         {
-            Player1.Deck.Shuffle();
-            Player2.Deck.Shuffle();
+            StartCoroutine(_beginGameSequence());
+        }
+
+        private IEnumerator _beginGameSequence()
+        {
             yield return new WaitForSeconds(2.4f);
             Player1.DrawInitialHand();
             Player2.DrawInitialHand();
 
             Player1.InitTurn();
         }
-        
         public void SpawnPlayer1()
         {
             // Player1 = ((GameObject)Instantiate(PlayerObject)).GetComponent<Player>();
@@ -30,32 +33,17 @@ namespace HarryPotterUnity.Game
             Player1.transform.parent = transform;
 
             Player1.OppositePlayer = Player2;
-
-            Player1.Deck.InitDeck(
-                DeckGenerator.GenerateDeck(new List<Lesson.LessonTypes>
-                {
-                    Lesson.LessonTypes.Creatures,
-                    Lesson.LessonTypes.Charms
-                }));
-
+            Player1.Deck.Shuffle();
         }
         public void SpawnPlayer2()
         {
            // Player2 = ((GameObject)Instantiate(PlayerObject)).GetComponent<Player>();
-
             Player2 = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.Euler(0f, 0f, 180f), 0).GetComponent<Player>();
             Player2.transform.localRotation = Quaternion.Euler(0f, 0f, 180f);
             Player2.transform.parent = transform;
 
             Player2.OppositePlayer = Player1;
-
-            Player2.Deck.InitDeck(
-                DeckGenerator.GenerateDeck(new List<Lesson.LessonTypes>
-                {
-                    Lesson.LessonTypes.Creatures,
-                    Lesson.LessonTypes.Charms,
-                    Lesson.LessonTypes.Transfiguration
-                }));
+            Player2.Deck.Shuffle();
         }
 
         public void DestroyPlayers()
