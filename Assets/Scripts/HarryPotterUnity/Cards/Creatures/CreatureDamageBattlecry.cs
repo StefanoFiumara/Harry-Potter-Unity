11a59dcad981cb4aaf78cfc14b9272504e1a1c18
Stefano Fiumara <sdf10517@gmail.com>
2015-03-12 13:46:28 -0500

    Added Black Bat

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Creatures/CreatureDamageBattlecry.cs
@@ -0,0 +1,20 @@
+﻿using HarryPotterUnity.Cards.Generic;
+using HarryPotterUnity.Cards.Interfaces;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Creatures
+{
+    class CreatureDamageBattlecry : GenericCreature, IPersistentCard
+    {
+        [UsedImplicitly, SerializeField] 
+        private int _battlecryDamage;
+
+        public new void OnEnterInPlayAction()
+        {
+            base.OnEnterInPlayAction();
+
+            Player.OppositePlayer.TakeDamage(_battlecryDamage);
+        }
+    }
+}
