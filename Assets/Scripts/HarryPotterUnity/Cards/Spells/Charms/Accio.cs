7684985137724fb16bb8df0ba6664bb337005e02
Stefano Fiumara <sdf10517@gmail.com>
2015-03-12 14:18:28 -0500

    Added Accio

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Spells/Charms/Accio.cs
@@ -0,0 +1,22 @@
+﻿using HarryPotterUnity.Cards.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Spells.Charms
+{
+    [UsedImplicitly]
+    public class Accio : GenericSpell {
+
+        protected override void OnPlayAction()
+        {
+            //TODO: Implement it with input instead?
+            var lessons = Player.Discard.GetCardsOfType(CardTypes.Lesson, 2);
+            foreach (var lesson in lessons)
+            {
+                Player.Discard.Remove(lesson);
+                Player.Hand.Add(lesson);
+            }
+
+        }
+    }
+}
