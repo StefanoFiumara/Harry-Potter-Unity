fa32f87ea72810a115a213080224c7232c993b2a
Stefano Fiumara <sdf10517@gmail.com>
2015-04-23 00:21:16 -0500

    Added Input Requirement

---
+++ Assets/Scripts/HarryPotterUnity/Cards/PlayRequirements/InputRequirement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using HarryPotterUnity.Cards.Generic;
+using HarryPotterUnity.Cards.Interfaces;
+using HarryPotterUnity.Game;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.PlayRequirements
+{
+    [UsedImplicitly]
+    public class InputRequirement : MonoBehaviour, ICardPlayRequirement
+    {
+
+        private GenericSpellRequiresInput _cardInfo;
+
+        [UsedImplicitly]
+        void Start()
+        {
+            _cardInfo = GetComponent<GenericSpellRequiresInput>();
+        }
+
+        public bool MeetsRequirement()
+        {
+            return _cardInfo.GetValidCards().Count >= _cardInfo.InputRequired;
+        }
+
+        public void OnRequirementMet() { }
+    }
+}
