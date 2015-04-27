8814450473731db1c6be5839ef9734a868762c81
Stefano Fiumara <sdf10517@gmail.com>
2015-04-27 18:44:40 -0500

    Added Marble and Sandstone Gargoyles

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Creatures/MarbleGargoyle.cs
@@ -0,0 +1,36 @@
+﻿using HarryPotterUnity.Cards.Generic;
+using HarryPotterUnity.Cards.Interfaces;
+using JetBrains.Annotations;
+
+namespace HarryPotterUnity.Cards.Creatures
+{
+    [UsedImplicitly]
+    public class MarbleGargoyle : GenericCreature, IPersistentCard {
+
+        public new void OnInPlayBeforeTurnAction()
+        {
+            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
+            {
+                Player.DamagePerTurn += 3;
+            }
+        }
+
+        public new void OnInPlayAfterTurnAction()
+        {
+            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
+            {
+                Player.DamagePerTurn -= 3;
+            }
+        }
+
+        public new void OnExitInPlayAction()
+        {
+            base.OnExitInPlayAction();
+
+            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
+            {
+                Player.DamagePerTurn -= 3;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HarryPotterUnity/Cards/Creatures/SandstoneGargoyle.cs
@@ -0,0 +1,36 @@
+﻿using HarryPotterUnity.Cards.Generic;
+using HarryPotterUnity.Cards.Interfaces;
+using JetBrains.Annotations;
+
+namespace HarryPotterUnity.Cards.Creatures
+{
+    [UsedImplicitly]
+    public class SandstoneGargoyle : GenericCreature, IPersistentCard {
+
+        public new void OnInPlayBeforeTurnAction()
+        {
+            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
+            {
+                Player.DamagePerTurn += 2;
+            }
+        }
+
+        public new void OnInPlayAfterTurnAction()
+        {
+            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
+            {
+                Player.DamagePerTurn -= 2;
+            }
+        }
+
+        public new void OnExitInPlayAction()
+        {
+            base.OnExitInPlayAction();
+
+            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
+            {
+                Player.DamagePerTurn -= 2;
+            }
+        }
+    }
+}
