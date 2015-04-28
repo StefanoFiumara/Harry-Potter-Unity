6aea0eb718e4a1ee532382155f838d4544ba0ab4
Stefano Fiumara <sdf10517@gmail.com>
2015-04-27 19:03:32 -0500

    Fixed gargoyles subtracting DamagePerTurn when they are played

--- Assets/Scripts/HarryPotterUnity/Cards/Creatures/MarbleGargoyle.cs
+++ Assets/Scripts/HarryPotterUnity/Cards/Creatures/MarbleGargoyle.cs
@@ -5,32 +5,34 @@ using JetBrains.Annotations;
 namespace HarryPotterUnity.Cards.Creatures
 {
     [UsedImplicitly]
-    public class MarbleGargoyle : GenericCreature, IPersistentCard {
+    public class MarbleGargoyle : GenericCreature, IPersistentCard 
+    {
+        private bool _hasAddedDamage;
 
         public new void OnInPlayBeforeTurnAction()
         {
-            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
-            {
-                Player.DamagePerTurn += 3;
-            }
+            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count != 0) return;
+
+            Player.DamagePerTurn += 3;
+            _hasAddedDamage = true;
         }
 
         public new void OnInPlayAfterTurnAction()
         {
-            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
-            {
-                Player.DamagePerTurn -= 3;
-            }
+            if (!_hasAddedDamage) return;
+
+            Player.DamagePerTurn -= 3;
+            _hasAddedDamage = false;
         }
 
         public new void OnExitInPlayAction()
         {
             base.OnExitInPlayAction();
 
-            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
-            {
-                Player.DamagePerTurn -= 3;
-            }
+            if (!_hasAddedDamage) return;
+
+            Player.DamagePerTurn -= 3;
+            _hasAddedDamage = false;
         }
     }
 }
--- Assets/Scripts/HarryPotterUnity/Cards/Creatures/SandstoneGargoyle.cs
+++ Assets/Scripts/HarryPotterUnity/Cards/Creatures/SandstoneGargoyle.cs
@@ -1,36 +1,40 @@
 ﻿using HarryPotterUnity.Cards.Generic;
 using HarryPotterUnity.Cards.Interfaces;
 using JetBrains.Annotations;
+using UnityEngine;
+
 
 namespace HarryPotterUnity.Cards.Creatures
 {
     [UsedImplicitly]
-    public class SandstoneGargoyle : GenericCreature, IPersistentCard {
+    public class SandstoneGargoyle : GenericCreature, IPersistentCard
+    {
+        private bool _hasAddedDamage;
 
         public new void OnInPlayBeforeTurnAction()
         {
-            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
-            {
-                Player.DamagePerTurn += 2;
-            }
+            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count != 0) return;
+
+            Player.DamagePerTurn += 2;
+            _hasAddedDamage = true;
         }
 
         public new void OnInPlayAfterTurnAction()
         {
-            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
-            {
-                Player.DamagePerTurn -= 2;
-            }
+            if (!_hasAddedDamage) return;
+
+            Player.DamagePerTurn -= 2;
+            _hasAddedDamage = false;
         }
 
         public new void OnExitInPlayAction()
         {
             base.OnExitInPlayAction();
 
-            if (Player.OppositePlayer.InPlay.GetCreaturesInPlay().Count == 0)
-            {
-                Player.DamagePerTurn -= 2;
-            }
+            if (!_hasAddedDamage) return;
+
+            Player.DamagePerTurn -= 2;
+            _hasAddedDamage = false;
         }
     }
 }
