52df9099c3da8e134777669216eab730c1d84847
Stefano Fiumara <sdf10517@gmail.com>
2015-04-29 19:43:54 -0500

    Added Endless Sandwiches

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Spells/Charms/EndlessSandwiches.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Cards.Generic;
+using JetBrains.Annotations;
+
+namespace HarryPotterUnity.Cards.Spells.Charms
+{
+    [UsedImplicitly]
+    public class EndlessSandwiches : GenericSpell {
+        protected override void SpellAction(List<GenericCard> targets)
+        {
+            while (Player.Hand.Cards.Count < 7)
+            {
+                Player.Deck.DrawCard();
+            }
+        }
+    }
+}
