19bdd1592f0bfee7445f628c6177823d03014f10
Stefano Fiumara <sdf10517@gmail.com>
2015-08-08 21:35:21 -0500

    Added Deck Requirements to wands

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Generic/DeckGenerationRequirements/DeckContainsCardWithNameRequirement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Cards.Generic.Interfaces;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Generic.DeckGenerationRequirements
+{
+    public class DeckContainsCardWithNameRequirement : MonoBehaviour, IDeckGenerationRequirement
+    {
+
+        [SerializeField, UsedImplicitly] private string _cardName;
+
+        public bool MeetsRequirement(List<GenericCard> currentDeck)
+        {
+            return currentDeck.Exists(card => card.CardName.Contains(_cardName));
+        }
+    }
+}
