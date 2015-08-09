7cd8943e210fa5b59c6cf4699a5f12772ec61e56
Stefano Fiumara <sdf10517@gmail.com>
2015-08-08 20:02:12 -0500

    Add Dragon Heart Wand and Phoenix Feather Wand

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Generic/PlayRequirements/CardInPlayProvidesLessonsRequirement.cs
@@ -0,0 +1,34 @@
+﻿using HarryPotterUnity.Cards.Generic.Interfaces;
+using HarryPotterUnity.Game;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Generic.PlayRequirements
+{
+    public class CardInPlayProvidesLessonsRequirement : MonoBehaviour, ICardPlayRequirement
+    {
+
+        private Player _player;
+
+        [UsedImplicitly, SerializeField]
+        private Lesson.LessonTypes _lessonType;
+
+        [UsedImplicitly, SerializeField]
+        private int _amountProvided;
+    
+        [UsedImplicitly]
+        void Start()
+        {
+            _player = GetComponent<GenericCard>().Player;
+        }
+
+        public bool MeetsRequirement()
+        {
+            return _player.InPlay.Cards.Exists(card => card is ILessonProvider &&
+                                                       ((ILessonProvider) card).AmountLessonsProvided >= _amountProvided &&
+                                                       ((ILessonProvider) card).LessonType == _lessonType);
+        }
+
+        public void OnRequirementMet() { }
+    }
+}
