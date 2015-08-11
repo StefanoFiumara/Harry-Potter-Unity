b12ef79c7c0d21bf20d75daef99528d00fb69cfa
Stefano Fiumara <sdf10517@gmail.com>
2015-08-11 18:02:01 -0500

    Add Card Limit Deck requirement

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Generic/DeckGenerationRequirements/DeckCardLimitRequirement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Cards.Generic.Interfaces;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Generic.DeckGenerationRequirements
+{
+    public class DeckCardLimitRequirement : MonoBehaviour, IDeckGenerationRequirement
+    {
+        [UsedImplicitly, SerializeField] private int _maximumAmountAllowed;
+
+        private GenericCard _cardInfo;
+
+        [UsedImplicitly]
+        private void Start()
+        {
+            _cardInfo = GetComponent<GenericCard>();
+        }
+
+        public bool MeetsRequirement(List<GenericCard> currentDeck)
+        {
+            return currentDeck.Count(c => c.Equals(_cardInfo)) < _maximumAmountAllowed;
+        }
+    }
+}
