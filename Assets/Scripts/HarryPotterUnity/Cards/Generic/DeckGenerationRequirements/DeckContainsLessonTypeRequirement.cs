cd4daa6d413e9b20f0fd76ecccfc35174479b4d2
Stefano Fiumara <sdf10517@gmail.com>
2015-08-11 19:01:40 -0500

    Add Lesson Type Deck Requirement

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Generic/DeckGenerationRequirements/DeckContainsLessonTypeRequirement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Cards.Generic.Interfaces;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Generic.DeckGenerationRequirements
+{
+    public class DeckContainsLessonTypeRequirement : MonoBehaviour, IDeckGenerationRequirement
+    {
+        [SerializeField, UsedImplicitly] private LessonTypes _type;
+
+        public bool MeetsRequirement(List<GenericCard> currentDeck)
+        {
+            return currentDeck.Exists(c => c is ILessonProvider && ((ILessonProvider)c).LessonType == _type);
+        }
+    }
+}
