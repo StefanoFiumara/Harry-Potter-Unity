09fef4f761470155aee30d46cdd89aa24837c50f
Stefano Fiumara <sdf10517@gmail.com>
2015-08-29 01:16:47 -0500

    Add Hermione Granger as a starting character

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Characters/HermioneGranger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Cards.Generic;
+using HarryPotterUnity.Cards.Generic.Interfaces;
+using UnityEngine;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace HarryPotterUnity.Cards.Characters
+{
+    [UsedImplicitly]
+    public class HermioneGranger : GenericCard, IPersistentCard
+    {
+        public bool CanPerformInPlayAction()
+        {
+            return Player.CanUseActions() && 
+                   Player.Hand.Cards.Count(c => c.Type == CardType.Lesson) >= 2 &&
+                   Player.AmountLessonsInPlay >= 2;
+        }
+
+        public void OnSelectedAction()
+        {
+            var firstLesson = Player.Hand.Cards.Find(c => c.Type == CardType.Lesson);
+            var secondLesson = Player.Hand.Cards.FindLast(c => c.Type == CardType.Lesson);
+
+            Player.InPlay.Add(firstLesson);
+            Player.InPlay.Add(secondLesson);
+            Player.Hand.RemoveAll(new[] {firstLesson, secondLesson});
+
+            Player.UseActions();
+        }
+
+        protected override void OnClickAction(List<GenericCard> targets) { }
+        public void OnInPlayBeforeTurnAction() { }
+        public void OnInPlayAfterTurnAction() { }        
+        public void OnEnterInPlayAction() { }
+        public void OnExitInPlayAction() { }
+    }
+}
