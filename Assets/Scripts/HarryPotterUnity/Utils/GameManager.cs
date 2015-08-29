21a782732e23b95c280a575b57b14a61edb4c994
Stefano Fiumara <sdf10517@gmail.com>
2015-08-29 01:12:18 -0500

    Rename some variables for consistency

--- Assets/Scripts/HarryPotterUnity/Utils/GameManager.cs
+++ Assets/Scripts/HarryPotterUnity/Utils/GameManager.cs
@@ -7,16 +7,17 @@ namespace HarryPotterUnity.Utils
 {
     public static class GameManager {
 
-        public const int PreviewLayer = 9;
-        public const int CardLayer = 10;
-        public const int ValidChoiceLayer = 11;
-        public const int IgnoreRaycastLayer = 2;
-        public const int DeckLayer = 12;
+        public const int PREVIEW_LAYER = 9;
+        public const int CARD_LAYER = 10;
+        public const int VALID_CHOICE_LAYER = 11;
+        public const int IGNORE_RAYCAST_LAYER = 2;
+        public const int DECK_LAYER = 12;
 
-        public static byte NetworkIdCounter;
+        public static byte _networkIdCounter;
+
         public static readonly List<GenericCard> AllCards = new List<GenericCard>(); 
 
-        public static Camera PreviewCamera;
+        public static Camera _previewCamera;
         
         public static readonly TweenQueue TweenQueue = new TweenQueue();
 
