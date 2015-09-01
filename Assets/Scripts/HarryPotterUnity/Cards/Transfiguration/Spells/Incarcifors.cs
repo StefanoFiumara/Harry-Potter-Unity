ec23dcc1a0098948b6a1b2acbe4cbc999d71a39c
Stefano Fiumara <sdf10517@gmail.com>
2015-08-31 21:34:02 -0500

    Add Incarcifors

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Transfiguration/Spells/Incarcifors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Cards.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Transfiguration.Spells
+{
+    [UsedImplicitly]
+    public class Incarcifors : GenericSpell
+    {
+        public override List<GenericCard> GetValidTargets()
+        {
+            return Player.OppositePlayer.InPlay.GetCreaturesInPlay();
+        }
+
+        protected override void SpellAction(List<GenericCard> targets)
+        {
+            if (targets.Count != 1)
+            {
+                Debug.LogError("More or less than one input sent to Incarcifors");
+            }
+
+            var target = targets[0];
+
+            target.Enable();
+            Player.OppositePlayer.Discard.Add(target);
+            Player.OppositePlayer.InPlay.Remove(target);
+        }
+    }
+}
