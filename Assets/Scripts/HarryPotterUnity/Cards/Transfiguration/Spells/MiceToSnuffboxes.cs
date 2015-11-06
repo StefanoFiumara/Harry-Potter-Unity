2fdef057758868f2a298883905c6395d13f8ed8b
Stefano Fiumara <sdf10517@gmail.com>
2015-11-06 15:16:27 -0600

    Check if targets belong to the same player for animation purposes

--- Assets/Scripts/HarryPotterUnity/Cards/Transfiguration/Spells/MiceToSnuffboxes.cs
+++ Assets/Scripts/HarryPotterUnity/Cards/Transfiguration/Spells/MiceToSnuffboxes.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 
 namespace HarryPotterUnity.Cards.Transfiguration.Spells
@@ -15,13 +16,22 @@ namespace HarryPotterUnity.Cards.Transfiguration.Spells
 
         protected override void SpellAction(List<BaseCard> selectedCards)
         {
-            //TODO: fix animation bugs by doing an AddAll if the selected cards belong to the same player.
-            foreach(var card in selectedCards) {
-                card.Player.Hand.Add(card, preview: false);
-                card.Player.InPlay.Remove(card);
+            //Check if the cards belong to the same player and do and AddAll to prevent animation bugs
+            bool localPlayer = selectedCards.First().Player.IsLocalPlayer;
+            if (selectedCards.All(c => c.Player.IsLocalPlayer == localPlayer))
+            {
+                selectedCards.First().Player.Hand.AddAll(selectedCards);
+                selectedCards.First().Player.InPlay.RemoveAll(selectedCards);
+
             }
-
-
+            else
+            {
+                foreach (var card in selectedCards)
+                {
+                    card.Player.Hand.Add(card, preview: false);
+                    card.Player.InPlay.Remove(card);
+                }
+            }
         }
     }
 }
