0aa60fb026647f9f00b377c3a8939e34d09c815b
Stefano Fiumara <sdf10517@gmail.com>
2015-11-12 14:57:01 -0600

    Add a new deck requirement

---
+++ Assets/Scripts/HarryPotterUnity/DeckGeneration/Requirements/DeckDoesNotContainCardWithNameRequirement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Cards;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.DeckGeneration.Requirements
+{
+    [UsedImplicitly]
+    public class DeckDoesNotContainCardWithNameRequirement : MonoBehaviour, IDeckGenerationRequirement
+    {
+        [SerializeField, UsedImplicitly]
+        private string _cardName;
+
+        public bool MeetsRequirement(List<BaseCard> currentDeck)
+        {
+            return !currentDeck.Exists(card => card.CardName.Contains(_cardName));
+        }
+    }
+}
