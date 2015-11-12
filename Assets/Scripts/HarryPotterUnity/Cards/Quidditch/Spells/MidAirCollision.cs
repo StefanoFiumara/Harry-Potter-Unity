1c2a1be1100a6358ad81a3098b05d5e3bde4633d
Stefano Fiumara <sdf10517@gmail.com>
2015-11-12 14:58:47 -0600

    Add mid air collision

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/MidAirCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace HarryPotterUnity.Cards.Quidditch.Spells
+{
+    [UsedImplicitly]
+    public class MidAirCollision : BaseSpell
+    {
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            var target = targets.Single();
+
+            Player.Discard.Add(target);
+            Player.InPlay.Remove(target);
+            
+            Player.OppositePlayer.TakeDamage(10);
+        }
+
+        public override List<BaseCard> GetValidTargets()
+        {
+            return Player.InPlay.CardsExceptStartingCharacter;
+        }
+    }
+}
