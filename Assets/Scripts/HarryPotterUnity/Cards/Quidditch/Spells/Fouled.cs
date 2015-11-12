e5aa7288904944cd2244fcf123751bbb6909409c
Stefano Fiumara <sdf10517@gmail.com>
2015-11-12 14:59:01 -0600

    Add Fouled

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/Fouled.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace HarryPotterUnity.Cards.Quidditch.Spells
+{
+    [UsedImplicitly]
+    public class Fouled : BaseSpell
+    {
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            Player.OppositePlayer.TakeDamage(4);
+            Player.OppositePlayer.OnTurnStartEvent += () => Player.OppositePlayer.AddActions(-1);
+        }
+    }
+}
