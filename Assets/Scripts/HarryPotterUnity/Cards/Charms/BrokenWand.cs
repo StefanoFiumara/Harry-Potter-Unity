9688b0e0d27f7fa718ddb1f8d9b8d2656b2bbbcc
Stefano Fiumara <sdf10517@gmail.com>
2015-11-17 16:44:24 -0600

    Add Broken Wand

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Charms/BrokenWand.cs
@@ -0,0 +1,30 @@
+﻿using HarryPotterUnity.Cards.BasicBehavior;
+using JetBrains.Annotations;
+
+namespace HarryPotterUnity.Cards.Charms
+{
+    [UsedImplicitly]
+    public class BrokenWand : ItemLessonProvider
+    {
+        public override void OnSelectedAction()
+        {
+            var card = Player.Deck.TakeTopCard();
+
+            if (card is BaseLesson)
+            {
+                Player.InPlay.Add(card);
+            }
+            else
+            {
+                Player.Discard.Add(card);
+            }
+
+            Player.UseActions();
+        }
+
+        public override bool CanPerformInPlayAction()
+        {
+            return Player.CanUseActions();
+        }
+    }
+}
