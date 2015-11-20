e1c89108f24b4b2348c2fdfb5f2d6a74bbd06759
Stefano Fiumara <sdf10517@gmail.com>
2015-11-20 10:09:27 -0600

    Add Loop The Loops

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/LoopTheLoops.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Quidditch.Spells
+{
+    [UsedImplicitly]
+    public class LoopTheLoops : BaseSpell
+    {
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            Player.OppositePlayer.TakeDamage(4);
+
+            Player.Deck.DrawCard();
+            Player.Deck.DrawCard();
+            Player.Deck.DrawCard();
+            Player.Deck.DrawCard();
+        }
+    }
+}
