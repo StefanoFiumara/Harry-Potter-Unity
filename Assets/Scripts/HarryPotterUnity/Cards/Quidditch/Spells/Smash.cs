fc45a3e27da96bf719b3d4d0b19f4952622cfb4b
Stefano Fiumara <sdf10517@gmail.com>
2015-11-20 09:45:49 -0600

    Add Smash

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/Smash.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Enums;
+using JetBrains.Annotations;
+
+namespace HarryPotterUnity.Cards.Quidditch.Spells
+{
+    [UsedImplicitly]
+    public class Smash : BaseSpell 
+    {
+        public override List<BaseCard> GetValidTargets()
+        {
+            return Player.InPlay.CardsExceptStartingCharacter.Where(c => c.Type == Type.Item).ToList();
+        }
+
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            var target = targets.Single();
+            Player.OppositePlayer.Discard.Add(target);
+        }
+    }
+}
