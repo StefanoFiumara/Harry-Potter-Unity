dc6d910bd797e7250adb200e93e2f33af45fc567
Stefano Fiumara <sdf10517@gmail.com>
2015-11-22 07:16:48 -0600

    Fix Smash targeting wrong cards

--- Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/Smash.cs
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/Smash.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using HarryPotterUnity.Cards.PlayRequirements;
 using HarryPotterUnity.Enums;
 using JetBrains.Annotations;
+using UnityEngine;
 
 namespace HarryPotterUnity.Cards.Quidditch.Spells
 {
     [UsedImplicitly]
+    [RequireComponent(typeof(InputRequirement))]
     public class Smash : BaseSpell 
     {
         public override List<BaseCard> GetValidTargets()
         {
-            return Player.InPlay.CardsExceptStartingCharacter.Where(c => c.Type == Type.Item).ToList();
+            return Player.OppositePlayer.InPlay.CardsExceptStartingCharacter.Where(c => c.Type == Type.Item).ToList();
         }
 
         protected override void SpellAction(List<BaseCard> targets)
