c5cc9226d66776b9252b55a1babec27e42e949e9
Stefano Fiumara <sdf10517@gmail.com>
2015-12-16 14:27:19 -0600

    Add missing behavior to Lesson Book

---
+++ Assets/Scripts/HarryPotterUnity/Cards/BasicBehavior/ItemLessonBook.cs
@@ -0,0 +1,18 @@
+﻿namespace HarryPotterUnity.Cards.BasicBehavior
+{
+    public class ItemLessonBook : ItemLessonProvider {
+        public override bool CanPerformInPlayAction()
+        {
+            return Player.CanUseActions();
+        }
+
+        public override void OnSelectedAction()
+        {
+            Player.Discard.Add(this);
+
+            Player.Deck.DrawCard();
+            Player.Deck.DrawCard();
+            Player.Deck.DrawCard();
+        }
+    }
+}
