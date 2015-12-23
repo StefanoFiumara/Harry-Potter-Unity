76120e513bcd4f653c853d2c8a3d7112c60f7e34
Stefano Fiumara <sdf10517@gmail.com>
2015-12-22 20:11:21 -0600

    comment uneeded photon warning

--- Assets/Photon Unity Networking/Plugins/PhotonNetwork/NetworkingPeer.cs
+++ Assets/Photon Unity Networking/Plugins/PhotonNetwork/NetworkingPeer.cs
@@ -1150,7 +1150,7 @@ internal class NetworkingPeer : LoadbalancingPeer, IPhotonPeerListener
             }
             else if (operationResponse.ReturnCode == ErrorCode.NoRandomMatchFound)
             {
-                Debug.LogWarning("Operation failed: " + operationResponse.ToStringFull());
+                //Debug.LogWarning("Operation failed: " + operationResponse.ToStringFull());
             }
             else
             {
