745a3316b5445d301d2874b0d84f042b51c6c88b
Stefano Fiumara <sdf10517@gmail.com>
2015-12-28 20:42:08 -0600

    Add Gringotts

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Charms/Locations/Gringotts.cs
@@ -0,0 +1,20 @@
+﻿namespace HarryPotterUnity.Cards.Charms.Locations
+{
+    public class Gringotts : BaseLocation
+    {
+        public override void OnInPlayBeforeTurnAction()
+        {
+            Player.AddActions(1);
+        }
+
+        public override void OnEnterInPlayAction()
+        {
+            Player.AddActions(1);
+        }
+
+        public override void OnInPlayAfterTurnAction()
+        {
+            Player.OppositePlayer.AddActions(1);
+        }
+    }
+}
