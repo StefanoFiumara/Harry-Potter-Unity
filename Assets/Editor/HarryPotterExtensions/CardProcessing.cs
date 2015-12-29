f5b314db41734bbb29e73bd4fca07066b3de3d29
Stefano Fiumara <sdf10517@gmail.com>
2015-12-29 14:32:20 -0600

    adjust card processing template

--- Assets/Editor/HarryPotterExtensions/CardProcessing.cs
+++ Assets/Editor/HarryPotterExtensions/CardProcessing.cs
@@ -1,28 +1,27 @@
-﻿using JetBrains.Annotations;
+﻿using System.Linq;
+using HarryPotterUnity.DeckGeneration.Requirements;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
 
 namespace HarryPotterExtensions
 {
-    public static class CardProcessing {
-
-        /*[MenuItem("Harry Potter TCG/Do Work")] */
+    public static class CardProcessing
+    {    
         /// <summary>
         /// Template for applying changes to all card objects in the library
         /// </summary>
         [UsedImplicitly]
+        //[MenuItem("Harry Potter TCG/Find DeckContainsCardWithNameRequirement")]
         public static void DoWork()
         {
-            /*
-        var assetFolderPaths = AssetDatabase.GetAllAssetPaths().Where(path => path.EndsWith(".prefab") && path.Contains("/Cards/"));
+            
+            //var assetFolderPaths = AssetDatabase.GetAllAssetPaths().Where(path => path.EndsWith(".prefab") && path.Contains("/Cards/"));
 
-        foreach (string path in assetFolderPaths)
-        {
-            var obj = (GameObject) AssetDatabase.LoadAssetAtPath(path, typeof(GameObject));
-
-            //Do work with asset...
-
-            AssetDatabase.SaveAssets();
-        }
-        */
+            //foreach (string path in assetFolderPaths)
+            //{
+            //    var obj = (GameObject) AssetDatabase.LoadAssetAtPath(path, typeof(GameObject));
+            //}
         }
     }
 }
