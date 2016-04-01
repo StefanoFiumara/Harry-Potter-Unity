d3e2b5619ce91d0043c74859b7c8f4b6eba840b7
Stefano Fiumara <sdf10517@gmail.com>
2016-04-01 15:54:18 -0500

    Add Gryffindor Match

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Matches/GryffindorMatch.cs
@@ -0,0 +1,12 @@
+﻿using HarryPotterUnity.Game;
+
+namespace HarryPotterUnity.Cards.Quidditch.Matches
+{
+    public class GryffindorMatch : BaseMatch
+    {
+        protected override void OnPlayerHasWonMatch(Player winner, Player loser)
+        {
+            winner.OnNextTurnStart += () => winner.AddActions(2);
+        }
+    }
+}
