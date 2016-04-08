506d0275d060a349bc45ae30fe152f04bb5148bb
Stefano Fiumara <sdf10517@gmail.com>
2016-04-08 15:02:52 -0500

    Add Venomous Tentacula

---
+++ Assets/Scripts/HarryPotterUnity/Cards/CareOfMagicalCreatures/Creatures/VenomousTentacula.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Enums;
+using HarryPotterUnity.Utils;
+
+namespace HarryPotterUnity.Cards.CareOfMagicalCreatures.Creatures
+{
+    public class VenomousTentacula : BaseCreature
+    {
+        public override bool CanPerformInPlayAction()
+        {
+            return Player.Discard.LessonsOfType(LessonTypes.Potions).Any();
+        }
+
+        public override void OnSelectedAction(List<BaseCard> targets = null)
+        {
+            Player.Discard.Add(this);
+
+            var lesson = Player.Discard.LessonsOfType(LessonTypes.Potions).First();
+
+            Player.InPlay.Add(lesson);
+        }
+    }
+}
