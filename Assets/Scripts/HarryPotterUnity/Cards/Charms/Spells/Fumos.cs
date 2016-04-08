2800adba075d088c55ba2a326d6ca0bfa6a03bce
Stefano Fiumara <sdf10517@gmail.com>
2016-04-08 15:37:38 -0500

    add Fumos

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Charms/Spells/Fumos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Cards.Interfaces;
+
+namespace HarryPotterUnity.Cards.Charms.Spells
+{
+    public class Fumos : BaseSpell, IDamageSpell
+    {
+        public int DamageAmount { get; set; }
+
+        protected override void Start()
+        {
+            base.Start();
+            DamageAmount = 2;
+        }
+
+        //TODO: test this
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            var allCreatures = Player.InPlay.Creatures.Concat(Player.OppositePlayer.InPlay.Creatures);
+
+            foreach (var creature in allCreatures)
+            {
+                ((BaseCreature)creature).TakeDamage(DamageAmount);
+            }
+        }
+    }
+}
