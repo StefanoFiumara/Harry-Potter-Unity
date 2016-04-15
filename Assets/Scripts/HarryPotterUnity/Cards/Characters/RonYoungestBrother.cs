a36bccf581f039fd92af1c21abd4fce6de1fbdce
Stefano Fiumara <sdf10517@gmail.com>
2016-04-15 14:58:13 -0500

    Add Ron, Youngest Brother

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Characters/RonYoungestBrother.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace HarryPotterUnity.Cards.Characters
+{
+    public class RonYoungestBrother : BaseCharacter
+    {
+        public override bool CanPerformInPlayAction()
+        {
+            return Player.Hand.Cards.Count == 0
+                && Player.IsLocalPlayer
+                && Player.CanUseActions();
+        }
+
+        public override void OnSelectedAction(List<BaseCard> targets = null)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Player.Deck.DrawCard();
+            }
+
+            Player.UseActions();
+        }
+    }
+}
