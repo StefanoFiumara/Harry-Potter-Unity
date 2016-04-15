bab44d575aa119c33d1f7f8bd924662158904239
Stefano Fiumara <sdf10517@gmail.com>
2016-04-15 15:45:28 -0500

    Add Draco Malfoy, Slytherin

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Characters/DracoMalfoySlytherin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Enums;
+
+namespace HarryPotterUnity.Cards.Characters
+{
+    public class DracoMalfoySlytherin : BaseCharacter
+    {
+        private bool HasEffectActivated { get; set; }
+
+        public override void OnEnterInPlayAction()
+        {
+            Player.OnCardPlayed += AddActionOnItemPlayed;
+        }
+
+        private void AddActionOnItemPlayed(BaseCard card, List<BaseCard> targets)
+        {
+            if (HasEffectActivated == false && card.Type == Type.Item)
+            {
+                HasEffectActivated = true;
+
+                Player.AddActions(1);
+            }
+        }
+
+        public override void OnExitInPlayAction()
+        {
+            HasEffectActivated = false;
+
+            Player.OnCardPlayed -= AddActionOnItemPlayed;
+        }
+
+        public override void OnInPlayAfterTurnAction()
+        {
+            HasEffectActivated = false;
+        }
+    }
+}
