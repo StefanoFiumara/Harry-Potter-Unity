0c688044447842e78b1adb7112a3365c988fd932
Stefano Fiumara <sdf10517@gmail.com>
2016-04-19 12:18:18 -0500

    Add Philosopher's Stone

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Transfiguration/Items/PhilosophersStone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Enums;
+
+namespace HarryPotterUnity.Cards.Transfiguration.Items
+{
+    public class PhilosophersStone : BaseItem
+    {
+        public override bool CanPerformInPlayAction()
+        {
+            return Player.CanUseActions(2) && Player.IsLocalPlayer;
+        }
+
+        public override void OnInPlayAction(List<BaseCard> targets = null)
+        {
+            var allCards = Player.Discard.GetHealableCards();
+
+            var lessons = allCards.Where(c => c.Type == Type.Lesson);
+            var other = allCards.Where(c => c.Type != Type.Lesson);
+            
+            Player.InPlay.AddAll(lessons);
+            Player.Deck.AddAll(other);
+
+            Player.Deck.Shuffle();
+
+            Player.UseActions(2);
+        }
+    }
+}
