b8f5bad1f0c0924be210f962f5794dbaeda0f3fb
Stefano Fiumara <sdf10517@gmail.com>
2016-04-19 12:36:25 -0500

    Add Porcupine Robe

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Transfiguration/Items/PorcupineRobe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Enums;
+
+namespace HarryPotterUnity.Cards.Transfiguration.Items
+{
+    public class PorcupineRobe : BaseItem
+    {
+        private readonly List<BaseCard> _enemyCreatures = new List<BaseCard>();
+
+        //TODO: Test this
+        public override void OnInPlayBeforeTurnAction()
+        {
+            foreach (var creature in _enemyCreatures.Cast<BaseCreature>())
+            {
+                creature.TakeDamage(1);
+            }
+
+            _enemyCreatures.Clear();
+        }
+
+        public override void OnEnterInPlayAction()
+        {
+            Player.OnDamageTaken += CountCreatureDamage;
+        }
+
+        public override void OnExitInPlayAction()
+        {
+            Player.OnDamageTaken -= CountCreatureDamage;
+        }
+
+        private void CountCreatureDamage(BaseCard source, int amount)
+        {
+            if (source.Type == Type.Creature)
+            {
+                _enemyCreatures.Add(source);
+            }
+        }
+    }
+}
