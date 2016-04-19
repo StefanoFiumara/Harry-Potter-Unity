c5850005f5414dd57ceb5f284e9eac090c6c3acb
Stefano Fiumara <sdf10517@gmail.com>
2016-04-19 13:02:17 -0500

    Add Guide to Household Pets, fix directory structure for materials

---
+++ Assets/Scripts/HarryPotterUnity/Cards/CareOfMagicalCreatures/Items/GuideToHouseholdPests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Cards.BasicBehavior;
+using HarryPotterUnity.Cards.PlayRequirements;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.CareOfMagicalCreatures.Items
+{
+    [RequireComponent(typeof(InputRequirement))]
+    public class GuideToHouseholdPests : ItemLessonProvider
+    {
+        public override List<BaseCard> GetInPlayActionTargets()
+        {
+            return Player.OppositePlayer.InPlay.Creatures.Concat(Player.InPlay.Creatures).ToList();
+        }
+
+        public override bool CanPerformInPlayAction()
+        {
+            return Player.IsLocalPlayer
+                   && Player.CanUseActions();
+        }
+
+        //TODO: Test this
+        public override void OnInPlayAction(List<BaseCard> targets = null)
+        {
+            if (targets != null)
+            {
+                Player.Discard.Add(this);
+
+                var target = targets.First();
+                ((BaseCreature)target).TakeDamage(4);
+
+                Player.UseActions();
+            }
+        }
+    }
+}
