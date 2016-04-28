de1d6082db3aaf816d34839129a5583e03544a3f
Stefano Fiumara <sdf10517@gmail.com>
2016-04-28 16:30:40 -0500

    Add moaning Myrtle's Bathroom

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Potions/Locations/MoaningMyrtlesBathroom.cs
@@ -0,0 +1,25 @@
+﻿using HarryPotterUnity.Game;
+
+namespace HarryPotterUnity.Cards.Potions.Locations
+{
+    public class MoaningMyrtlesBathroom : BaseLocation
+    {
+        public override void OnInPlayBeforeTurnAction()
+        {
+            CheckCharacter(Player);
+        }
+
+        public override void OnInPlayAfterTurnAction()
+        {
+            CheckCharacter(Player.OppositePlayer);
+        }
+
+        private void CheckCharacter(Player p)
+        {
+            if (p.InPlay.Characters.Count >= 2)
+            {
+                p.AddActions(1);
+            }
+        }
+    }
+}
