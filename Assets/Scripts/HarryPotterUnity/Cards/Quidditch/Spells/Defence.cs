c166ec1828a8bf5f6a7d9f419203896726d7ee59
Stefano Fiumara <sdf10517@gmail.com>
2016-05-03 14:12:08 -0500

    Add Defence

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/Defence.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Enums;
+
+namespace HarryPotterUnity.Cards.Quidditch.Spells
+{
+    public class Defence : BaseSpell
+    {
+        //TODO: Test this
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            //Creature, Spell, Item, Location, Match, Adventure, Character
+            Player.TypeImmunity.Add(Type.Creature);
+            Player.TypeImmunity.Add(Type.Spell);
+            Player.TypeImmunity.Add(Type.Item);
+            Player.TypeImmunity.Add(Type.Location);
+            Player.TypeImmunity.Add(Type.Match);
+            Player.TypeImmunity.Add(Type.Adventure);
+            Player.TypeImmunity.Add(Type.Character);
+
+            Player.OnNextTurnStart += () =>
+            {
+                Player.TypeImmunity.Remove(Type.Creature);
+                Player.TypeImmunity.Remove(Type.Spell);
+                Player.TypeImmunity.Remove(Type.Item);
+                Player.TypeImmunity.Remove(Type.Location);
+                Player.TypeImmunity.Remove(Type.Match);
+                Player.TypeImmunity.Remove(Type.Adventure);
+                Player.TypeImmunity.Remove(Type.Character);
+            };
+        }
+    }
+}
