4fa6bf1746f92155f4faefe34aab09500441cd37
Stefano Fiumara <sdf10517@gmail.com>
2016-05-03 14:31:09 -0500

    Add searching for the snitch

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/SearchForTheSnitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Enums;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Quidditch.Spells
+{
+    public class SearchForTheSnitch : BaseSpell
+    {
+        //TODO: Test this
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            var cards =
+                Player.Deck.Cards.Where(
+                    c =>
+                        c.Classification == ClassificationTypes.Quidditch ||
+                        c is BaseLesson && ((BaseLesson) c).LessonType == LessonTypes.Quidditch).ToList();
+
+            if (cards.Count > 0)
+            {
+                var selectedCard = cards.Skip(Random.Range(0, cards.Count)).First();
+                Player.Hand.Add(selectedCard);
+            }
+            
+            Player.Deck.Shuffle();
+        }
+    }
+}
