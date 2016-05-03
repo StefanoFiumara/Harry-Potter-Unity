504b152dfccfbacde25c2f30dd6226cb8e4ffda1
Stefano Fiumara <sdf10517@gmail.com>
2016-05-03 16:18:50 -0500

    Add Quidditch Cup

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Items/QuidditchCup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Enums;
+
+namespace HarryPotterUnity.Cards.Quidditch.Items
+{
+    public class QuidditchCup : BaseItem
+    {
+
+        private bool HasEffectActivated { get; set; }
+
+        public override void OnEnterInPlayAction()
+        {
+            Player.OnCardPlayed += AddActionOnQuidditchCardPlayed;
+        }
+
+        private void AddActionOnQuidditchCardPlayed(BaseCard card, List<BaseCard> targets)
+        {
+            if (HasEffectActivated == false && card.Classification == ClassificationTypes.Quidditch)
+            {
+                HasEffectActivated = true;
+
+                Player.AddActions(1);
+            }
+        }
+
+        public override void OnExitInPlayAction()
+        {
+            HasEffectActivated = false;
+
+            Player.OnCardPlayed -= AddActionOnQuidditchCardPlayed;
+        }
+
+        public override void OnInPlayAfterTurnAction()
+        {
+            HasEffectActivated = false;
+        }
+    }
+}
