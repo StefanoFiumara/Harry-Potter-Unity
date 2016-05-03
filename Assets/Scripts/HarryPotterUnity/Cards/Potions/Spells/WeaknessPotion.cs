bd17e962433a1fd6e5d3f14361950771e51bbee9
Stefano Fiumara <sdf10517@gmail.com>
2016-05-03 11:20:10 -0500

    Add Weakness Potion

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Potions/Spells/WeaknessPotion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Cards.Interfaces;
+using HarryPotterUnity.Enums;
+
+namespace HarryPotterUnity.Cards.Potions.Spells
+{
+    public class WeaknessPotion : BaseSpell, IDamageSpell
+    {
+        public int DamageAmount { get; set; }
+
+        protected override void Start()
+        {
+            base.Start();
+            DamageAmount = 5;
+        }
+
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            Player.OppositePlayer.TakeDamage(this, DamageAmount);
+
+            Player.TypeImmunity.Add(Type.Creature);
+            Player.OnNextTurnStart += () => Player.TypeImmunity.Remove(Type.Creature);
+        }
+    }
+}
