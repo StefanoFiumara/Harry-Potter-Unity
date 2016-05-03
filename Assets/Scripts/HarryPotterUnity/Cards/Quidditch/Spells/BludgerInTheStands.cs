86e2981c5baf1b9d8b0e186cdfde605d16e2ff4e
Stefano Fiumara <sdf10517@gmail.com>
2016-05-03 15:12:23 -0500

    add Bludger in the Stands

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/BludgerInTheStands.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Cards.Interfaces;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Quidditch.Spells
+{
+    public class BludgerInTheStands : BaseSpell, IDamageSpell
+    {
+        public int DamageAmount { get; set; }
+
+        protected override void Start()
+        {
+            base.Start();
+            DamageAmount = 4;
+        }
+
+        //TODO: Test this
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            Player.OppositePlayer.TakeDamage(this, DamageAmount);
+
+            var item =
+                Player.OppositePlayer.InPlay.Items.Skip(Random.Range(0, Player.OppositePlayer.InPlay.Items.Count))
+                    .First();
+
+            Player.OppositePlayer.Discard.Add(item);
+
+            DamageAmount = 4;
+        }
+        
+    }
+}
