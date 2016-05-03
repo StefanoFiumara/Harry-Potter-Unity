2a918e7361ccf80881f4658bf0a1ad9c798bb1da
Stefano Fiumara <sdf10517@gmail.com>
2016-05-03 11:06:55 -0500

    Add Fire Protection Potion

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Potions/Spells/FireProtectionPotion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using HarryPotterUnity.Cards.Interfaces;
+using HarryPotterUnity.Enums;
+
+namespace HarryPotterUnity.Cards.Potions.Spells
+{
+    public class FireProtectionPotion : BaseSpell, IDamageSpell
+    {
+        public int DamageAmount { get; set; }
+
+        protected override void Start()
+        {
+            base.Start();
+            DamageAmount = 3;
+        }
+
+        //TODO: Test this
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            Player.OppositePlayer.TakeDamage(this, DamageAmount);
+
+            Player.TypeImmunity.Add(Type.Spell);
+            Player.OnNextTurnStart += () => Player.TypeImmunity.Remove(Type.Spell);
+        }
+    }
+}
