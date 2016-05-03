33cd3957ee36a2cbd8676c08f2ef4225181ed5f2
Stefano Fiumara <sdf10517@gmail.com>
2016-05-03 15:26:17 -0500

    Add Quick Thinking

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Quidditch/Spells/QuickThinking.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarryPotterUnity.Cards.Interfaces;
+using HarryPotterUnity.Cards.PlayRequirements;
+using UnityEngine;
+
+namespace HarryPotterUnity.Cards.Quidditch.Spells
+{
+    [RequireComponent(typeof(InputRequirement))]
+    public class QuickThinking : BaseSpell, IDamageSpell
+    {
+        public int DamageAmount { get; set; }
+
+        protected override void Start()
+        {
+            base.Start();
+            DamageAmount = 3;
+        }
+
+        public override List<BaseCard> GetFromHandActionTargets()
+        {
+            return new List<BaseCard> { Player.Deck.StartingCharacter, Player.OppositePlayer.Deck.StartingCharacter };
+        }
+
+        //TODO: Test this
+        protected override void SpellAction(List<BaseCard> targets)
+        {
+            var target = targets.Single();
+
+            if (target.Player.IsLocalPlayer)
+            {
+                Player.Deck.DrawCard();
+                Player.Deck.DrawCard();
+                Player.Deck.DrawCard();
+            }
+            else
+            {
+                Player.OppositePlayer.TakeDamage(this, DamageAmount);
+            }
+
+            DamageAmount = 3;
+        }
+    }
+}
