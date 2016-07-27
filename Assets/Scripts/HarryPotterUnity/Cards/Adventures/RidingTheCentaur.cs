b14e38c793705c6b41925d1515a4434e5856376a
Stefano Fiumara <sdf10517@gmail.com>
2016-07-27 11:48:39 -0500

    Add Riding the Centaur

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Adventures/RidingTheCentaur.cs
@@ -0,0 +1,36 @@
+﻿using HarryPotterUnity.Enums;
+
+namespace HarryPotterUnity.Cards.Adventures
+{
+    public class RidingTheCentaur : BaseAdventure
+    {
+        public override void OnInPlayAfterTurnAction()
+        {
+            Player.TypeImmunity.Add(Type.Creature);
+            Player.OnNextTurnStart += () => Player.TypeImmunity.Remove(Type.Creature);
+        }
+
+        protected override bool CanOpponentSolve()
+        {
+            return Player.OppositePlayer.InPlay.Cards.Count >= 4;
+
+        }
+
+        protected override void Solve()
+        {
+            int amountToReturn = 4;
+
+            for (int i = amountToReturn; i > 0; i--)
+            {
+                var card = Player.OppositePlayer.InPlay.GetRandomCard();
+
+                Player.OppositePlayer.Hand.Add(card);
+            }
+        }
+
+        protected override void Reward()
+        {
+            Player.OppositePlayer.Deck.DrawCard();
+        }
+    }
+}
