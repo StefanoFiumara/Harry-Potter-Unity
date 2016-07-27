41ee16e0b8ea59dd71462b9797b69ca62871b35f
Stefano Fiumara <sdf10517@gmail.com>
2016-07-27 13:51:48 -0500

    Add Troll in the Bathroom

---
+++ Assets/Scripts/HarryPotterUnity/Cards/Adventures/TrollInTheBathroom.cs
@@ -0,0 +1,32 @@
+﻿namespace HarryPotterUnity.Cards.Adventures
+{
+    public class TrollInTheBathroom : BaseAdventure
+    {
+        public override void OnInPlayBeforeTurnAction()
+        {
+            Player.OppositePlayer.TakeDamage(this, 2);
+        }
+
+        protected override bool CanOpponentSolve()
+        {
+            return Player.OppositePlayer.Hand.Cards.Count >= 7;
+        }
+
+        protected override void Solve()
+        {
+            int amountToDiscard = 7;
+
+            for (int i = amountToDiscard; i > 0; i--)
+            {
+                var card = Player.OppositePlayer.Hand.GetRandomCard();
+
+                Player.OppositePlayer.Discard.Add(card);
+            }
+        }
+
+        protected override void Reward()
+        {
+            Player.TakeDamage(this, 4);
+        }
+    }
+}
